f0e4d54b1bdc270c816303619f67b9f298bef599
ciornecki <ciornecki@icloud.com>
2023-05-16 23:50:11 +0200

    intro + skills done / todo: filtering and projects

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,15 @@ burgerIcon.addEventListener('click', () => {
 	burgerIcon.classList.toggle('active')
 	mnavList.classList.toggle('active')
 })
+
+const desktopToggler = document.querySelector('.desktop-themeToggler')
+
+desktopToggler.addEventListener('click', () => {
+	document.body.classList.toggle('light-mode')
+	themeIndicator.classList.toggle('active')
+	if (document.body.classList.contains('light-mode')) {
+		desktopToggler.innerHTML = `<i class='bx bx-sun' ></i>`
+	} else {
+		desktopToggler.innerHTML = `<i class='bx bx-moon' ></i>`
+	}
+})
