5c498e712fa66b1bc8c9dfe7ee9ff83e48cba8f4
ciornecki <ciornecki@icloud.com>
2023-05-18 22:39:27 +0200

    done

--- src/js/main.js
+++ src/js/main.js
@@ -25,3 +25,58 @@ desktopToggler.addEventListener('click', () => {
 		desktopToggler.innerHTML = `<i class='bx bx-moon' ></i>`
 	}
 })
+
+const filters = document.querySelectorAll('.filter')
+const projectCards = document.querySelectorAll('.project-card')
+
+window.onload = () => {
+	filters.forEach(el => {
+		if (el.classList.contains('all')) {
+			el.classList.add('active')
+		}
+	})
+	projectCards.forEach(el => {
+		el.classList.add('active')
+	})
+}
+filters.forEach(el => {
+	el.addEventListener('click', () => {
+		filters.forEach(filter => {
+			filter.classList.remove('active')
+		})
+		el.classList.add('active')
+		if (el.classList.contains('group')) {
+			projectCards.forEach(card => {
+				card.classList.remove('active')
+				if (card.classList.contains('group')) {
+					card.classList.add('active')
+				}
+			})
+		} else if (el.classList.contains('solo')) {
+			projectCards.forEach(card => {
+				card.classList.remove('active')
+				if (card.classList.contains('solo')) {
+					card.classList.add('active')
+				}
+			})
+		} else if (el.classList.contains('all')) {
+			projectCards.forEach(card => {
+				card.classList.add('active')
+			})
+		}
+	})
+})
+
+const sendBtn = document.querySelector('.send-btn')
+
+sendBtn.addEventListener('click', () => {
+	let fullName = document.querySelector('.name').value
+	let email = document.querySelector('.email').value
+	let message = document.querySelector('.message').value
+
+	if (fullName === '' || email === '' || message === '') {
+		return
+	} else {
+		window.location.href = `mailto:dawid.budzilek911@gmail.com?subject=${fullName}&body=${message}, Pozdrawiam`
+	}
+})
